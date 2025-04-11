5e054ddd7741eb76e12836a5de4ccc26f0588d30
Kim-JiYun <kimjiyunjane@naver.com>
2025-04-12 06:03:15 +0900

    [Feat] 서브타이틀 스테이지 맞게 전달

--- Frontend/src/components/Quest_element.tsx
+++ Frontend/src/components/Quest_element.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import {  Image, View, Text, TouchableOpacity} from "react-native";
 import questElementStyles from "../styles/questElementStyles";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../App";
+
 
 interface QuestElementProps {
   name: string;
+  subtitle?: string;
   onPress?: () => void;
 }
 
-export default function Quest_element({ name, onPress }: QuestElementProps) {
+export default function Quest_element({ name, subtitle }: QuestElementProps) {
+
+  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  
   return (
     <View style={questElementStyles.container}>
-      <TouchableOpacity onPress={onPress}>
+       <TouchableOpacity
+        onPress={() => {
+          if (subtitle) {
+            navigation.navigate("Quest_stage", { subtitle });
+          } else {
+            console.warn("subtitle이 없습니다!");
+          }
+        }}
+      >
         <Image
           source={require("../assets/Images/quest_icon.png")}
           style={questElementStyles.icon}
