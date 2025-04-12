d1c040efd2aad0fc88d5f291f7085ec8b93d915c
Kim-JiYun <kimjiyunjane@naver.com>
2025-04-12 12:21:34 +0900

    [Feat] 퀘스트 길 배치 및 미션 버튼 배치

--- Frontend/src/screens/Quest_stage.tsx
+++ Frontend/src/screens/Quest_stage.tsx
@@ -3,27 +3,49 @@ import { useRoute } from "@react-navigation/native";
 import Header_sky from "../components/Header_sky";
 import Quest_circle from "../components/Quest_circle";
 import Quest_street from "../components/Quest_street";
+import Quest_mission from "../components/Quest_mission";
 import questStyles from "../styles/questStyles";
 import questStageStyles from "../styles/questStageStyles";
 
-
 export default function Quest_stage() {
-    const route = useRoute();
-    const { subtitle } = route.params as { subtitle: string };
-    
-    return (
-        <View style={questStyles.container}>
-          <ScrollView contentContainerStyle={questStyles.scrollContainer}>
-            <View style={questStyles.headerWrapper}>
-              <Header_sky subtitle={subtitle}/>
-              <Quest_circle style={questStyles.circle} />
-              <Quest_street style={questStageStyles.street}/>
-              <Image
-                source={require("../assets/Images/goal.png")}
-                style={questStageStyles.goalImage}
-              />
-            </View>
-          </ScrollView>
+  const route = useRoute();
+  const { subtitle } = route.params as { subtitle: string };
+
+  return (
+    <View style={[questStyles.container, { flex: 1 }]}>
+      {/* ScrollView는 위 콘텐츠만 스크롤 */}
+      <ScrollView contentContainerStyle={questStyles.scrollContainer}>
+        <View style={questStyles.headerWrapper}>
+          <Header_sky subtitle={subtitle} />
+          <Quest_circle style={questStyles.circle} />
+          <Quest_street style={questStageStyles.street} />
+          <Image
+            source={require("../assets/Images/goal.png")}
+            style={questStageStyles.goalImage}
+          />
+
+          <View style={questStageStyles.stageOverlay}>
+            <Image
+              source={require("../assets/Images/stage_lock.png")}
+              style={[questStageStyles.stageWrapper, { alignSelf: "flex-start" }]}
+            />
+            <Image
+              source={require("../assets/Images/stage_lock.png")}
+              style={[questStageStyles.stageWrapper, { alignSelf: "flex-end" }]}
+            />
+            <Image
+              source={require("../assets/Images/stage_lock.png")}
+              style={[questStageStyles.stageWrapper, { alignSelf: "flex-start" }]}
+            />
+          </View>
         </View>
-    );
+      </ScrollView>
+
+      <View style={questStageStyles.missionWrapper}>
+        <Quest_mission
+          missiontitle="나를 돌아보기"
+        />
+      </View>
+    </View>
+  );
 }
