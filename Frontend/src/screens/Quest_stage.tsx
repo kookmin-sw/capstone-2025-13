2d9d6f4720f6571888b52c81b68235907394db85
Kim-JiYun <kimjiyunjane@naver.com>
2025-04-18 12:41:12 +0900

    [Feat] 퀘스트 스테이지 화면 리디자인

--- Frontend/src/screens/Quest_stage.tsx
+++ Frontend/src/screens/Quest_stage.tsx
@@ -3,6 +3,7 @@ import { useRoute } from "@react-navigation/native";
 import Header_sky from "../components/Header_sky";
 import Quest_circle from "../components/Darkgreen_circle";
 import Quest_street from "../components/Quest_street";
+import Stage_street from "../components/Stage_street";
 import Quest_mission from "../components/Quest_mission";
 import questStyles from "../styles/questStyles";
 import questStageStyles from "../styles/questStageStyles";
@@ -12,13 +13,13 @@ export default function Quest_stage() {
   const { subtitle } = route.params as { subtitle: string };
 
   return (
-    <View style={[questStyles.container, { flex: 1 }]}>
+    <View style={[questStyles.container, { flex: 1}]}>
       {/* ScrollView는 위 콘텐츠만 스크롤 */}
       <ScrollView contentContainerStyle={questStyles.scrollContainer}>
         <View style={questStyles.headerWrapper}>
           <Header_sky subtitle={subtitle} />
           <Quest_circle style={questStyles.circle} />
-          <Quest_street style={questStageStyles.street} />
+          <Stage_street style={questStageStyles.street} />
           <Image
             source={require("../assets/Images/goal.png")}
             style={questStageStyles.goalImage}
@@ -40,12 +41,15 @@ export default function Quest_stage() {
           </View>
         </View>
       </ScrollView>
-
-      <View style={questStageStyles.missionWrapper}>
+      </View>
+    );
+  }
+     
+     {/* 
+     <View style={questStageStyles.missionWrapper}>
         <Quest_mission
           missiontitle="나를 돌아보기"
         />
       </View>
     </View>
-  );
-}
+    */}
