9cccfedf3381fdb285eae49e5fdba5155ad57f26
Kim-JiYun <kimjiyunjane@naver.com>
2025-04-30 20:24:54 +0900

    [Feat] userinfo 화면 개발 및 연결

---
+++ Frontend/src/screens/UserInfo.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/homeStyles";
+import { View, ScrollView, Dimensions } from "react-native";
+
+export default function UserInfo() {
+    return (
+      <View style={styles.container}>
+        </View>
+    );
+}
