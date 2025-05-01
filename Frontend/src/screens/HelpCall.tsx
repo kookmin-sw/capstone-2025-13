b71423be7cc2ed9a1c8a55396943e21d2bfe738a
janeny117 <jsja031017@kookmin.ac.kr>
2025-05-01 16:16:15 +0900

    [Feat] HelpCall 화면에 버튼 추가 및 스크롤 기능 구현

--- Frontend/src/screens/HelpCall.tsx
+++ Frontend/src/screens/HelpCall.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet, Alert, Text, Platform } from "react-native";
+import { View, StyleSheet, Alert, Text, Platform, Button } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import * as Location from "expo-location";
+import { ScrollView } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function HelpCall() {
     const [location, setLocation] = useState<{
@@ -33,8 +35,19 @@ export default function HelpCall() {
 
     return (
         <View style={styles.container}>
+            <StatusBar style="auto" />
             <View style={styles.headerBox}>
                 <Text style={styles.headerText}>마음 케어 정보 지도</Text>
+                <ScrollView
+                    style={{ marginTop: 10 }}
+                    horizontal={true}
+                    contentContainerStyle={styles.scrollContainer}
+                >
+                    <Button title="Button 1" onPress={() => { }} />
+                    <Button title="Button 2" onPress={() => { }} />
+                    <Button title="Button 3" onPress={() => { }} />
+
+                </ScrollView>
             </View>
             <MapView
                 provider={PROVIDER_GOOGLE}
@@ -54,13 +67,10 @@ const styles = StyleSheet.create({
     },
     map: {
         flex: 1,
-        marginTop: "40%",
     },
     headerBox: {
-        position: "absolute",
-        top: 0,
         backgroundColor: "white",
-        padding: 10,
+        paddingHorizontal: 10,
         elevation: Platform.OS === "android" ? 10 : 5,
         shadowColor: "#000",
         shadowOffset: { width: 0, height: 5 },
@@ -70,10 +80,17 @@ const styles = StyleSheet.create({
         height: "20%",
         justifyContent: "center",
         zIndex: 10,
+        paddingTop: 40,
     },
+
     headerText: {
         fontWeight: "bold",
         fontSize: 16,
         textAlign: "center",
+    },
+
+    scrollContainer: {
+        flexDirection: "row",
+        alignItems: "center",
     },
 });
