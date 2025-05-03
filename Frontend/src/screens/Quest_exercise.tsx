2f12139e801fc4acfa8d4e6fb14dac711e57d1a4
Kim-JiYun <kimjiyunjane@naver.com>
2025-05-04 02:36:02 +0900

    [Feat] style 분리 #65

--- Frontend/src/screens/Quest_exercise.tsx
+++ Frontend/src/screens/Quest_exercise.tsx
@@ -6,6 +6,7 @@ import RecommendationList from "../components/RecommendationList";
 import { Pedometer } from 'expo-sensors';
 import fonts from '../constants/fonts';
 import { dynamic } from '../styles/questExerciseDynamicStyles';
+import { styles } from '../styles/questExerciseStyles';
 
 const { width } = Dimensions.get('window');
 
@@ -152,73 +153,3 @@ export default function QuestExercise() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-  },
-  scrollContent: {
-    padding: width * 0.05,
-    paddingBottom: width * 0.25,
-  },
-  title: {
-    color: '#fff',
-    marginTop: width * 0.03,
-  },
-  mission: {
-    color: '#fff',
-    fontWeight: 'bold',
-    marginVertical: width * 0.02,
-  },
-  progressChart: {
-    alignSelf: 'center',
-    marginTop: width * 0.05,
-  },
-  sectionText: {
-    color: '#fff',
-    marginVertical: width * 0.04,
-  },
-  uploadBox: {
-    backgroundColor: '#333',
-    height: width * 0.4,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 12,
-  },
-  uploadText: {
-    color: '#ccc',
-  },
-  uploadedImage: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 12,
-  },
-  buttonWrapper: {
-    position: 'absolute',
-    bottom: 20,
-    left: width * 0.05,
-    right: width * 0.05,
-  },
-  sectionTitle: {
-    fontFamily: fonts.laundryBold,
-    color: "#fff94f",
-    alignSelf: "flex-start",
-  },
-  completeButton: {
-    backgroundColor: '#FF3D89',
-    borderRadius: 50,
-    paddingVertical: width * 0.04,
-    alignItems: 'center',
-  },
-  chartContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'relative',
-    marginTop: width * 0.05,
-  },
-  centerTextContainer: {
-    position: 'absolute',
-    alignItems: 'center',
-  },
-});
