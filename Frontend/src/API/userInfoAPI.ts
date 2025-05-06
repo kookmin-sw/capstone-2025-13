d598963e571cbf626cc5250080df3000df6c796a
Kim-JiYun <kimjiyunjane@naver.com>
2025-05-06 16:44:16 +0900

    [Feat] API 필요한 내용 추가

--- Frontend/src/API/userInfoAPI.ts
+++ Frontend/src/API/userInfoAPI.ts
@@ -29,3 +29,59 @@ export const userInfoUpdate = async (password: string | null, nickname: string |
     }
 
 }
+
+export const getUserInfo = async () => {
+    try {
+        const response = await fetch("https://wuung.mori.space/auth/me", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "accept": "application/json",
+                "Authorization": `Bearer ${await AsyncStorage.getItem("accessToken")}`,
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error during user info update:", error);
+        throw error;
+    }
+
+}
+
+export const putProfileImg = async (profileImage: { uri: string; name: string; type: string }) => {
+    console.log("putProfileImg called with:", profileImage);
+    const formData = new FormData();
+    formData.append("multipartFile", {
+        uri: profileImage.uri,
+        name: profileImage.name,
+        type: profileImage.type,
+      } as any);
+  
+    try {
+      const response = await fetch("https://wuung.mori.space/auth/profile", {
+        method: "PUT",
+        headers: {
+          "accept": "application/json",
+          "Authorization": `Bearer ${await AsyncStorage.getItem("accessToken")}`,
+        },
+        body: formData,
+      });
+  
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Error during profile image update:", error);
+      throw error;
+    }
+  };
+  
