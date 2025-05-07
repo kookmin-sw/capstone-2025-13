87b0c505ce000d18a2e7c41a9a139c7bd93dcdec
Kim-JiYun <kimjiyunjane@naver.com>
2025-05-07 17:36:14 +0900

    [Feat] 뒤로 가기 버튼 스타일 추가 및 폰트 적용 #91

--- Frontend/src/styles/dailyTopicStyles.ts
+++ Frontend/src/styles/dailyTopicStyles.ts
@@ -1,5 +1,6 @@
 import { Dimensions, StyleSheet } from "react-native";
 import colors from "../constants/colors";
+import fonts from "../constants/fonts";
 
 const { width, height } = Dimensions.get("window");
 
@@ -7,6 +8,21 @@ const dailyTopicstyles = StyleSheet.create({
     container: {
         backgroundColor: "#EEF7E8",
         flex: 1,
+    },
+    backButtonWrapper: {
+        position: 'absolute',
+        left: width * 0.02,
+        top: width * 0.115,
+        padding: width * 0.02,
+        zIndex: 11,
+    },
+    headerText: {
+        fontFamily: fonts.semiBold,
+        top: width * 0.15,
+        marginBottom: width * 0.2,  
+        fontSize: width * 0.055,
+        textAlign: "center",
+        color: "#349C64",
     },
     scrollContainer: {
         padding: width * 0.04,
@@ -52,6 +68,7 @@ const dailyTopicstyles = StyleSheet.create({
         borderRadius: width * 0.1,
         paddingHorizontal: width * 0.03,
         backgroundColor: colors.white,
+        fontFamily: fonts.primary,
         flex: 1,
     },
     sendButton: {
@@ -65,9 +82,9 @@ const dailyTopicstyles = StyleSheet.create({
     },
     sendButtonText: {
         color: "#fff",
-        fontWeight: "bold",
         textAlign: "center",
         fontSize: width * 0.035,
+        fontFamily: fonts.semiBold,
     },
 });
 
