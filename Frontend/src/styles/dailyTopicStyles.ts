876352b630dc8db1e4a2d1c79ec08d0e5bbab9a0
Kim-JiYun <kimjiyunjane@naver.com>
2025-05-07 17:16:33 +0900

    [Feat] 반응형으로 구현 #91

--- Frontend/src/styles/dailyTopicStyles.ts
+++ Frontend/src/styles/dailyTopicStyles.ts
@@ -1,62 +1,65 @@
 import { Dimensions, StyleSheet } from "react-native";
 import colors from "../constants/colors";
+
 const { width, height } = Dimensions.get("window");
 
 const dailyTopicstyles = StyleSheet.create({
-    container:{
-        backgroundColor:"#EEF7E8",
+    container: {
+        backgroundColor: "#EEF7E8",
+        flex: 1,
     },
     scrollContainer: {
-        padding: 16,
+        padding: width * 0.04,
         marginVertical: height * 0.03,
     },
     questionContainer: {
         flexDirection: "row",
         alignItems: "flex-start",
-        marginBottom: 16,
+        marginBottom: height * 0.02,
     },
     answerContainer: {
         flexDirection: "row",
         justifyContent: "flex-end",
-        marginBottom: 16,
+        marginBottom: height * 0.02,
     },
     userProfileImage: {
-        width: 40,
-        height: 40,
-        marginLeft: 12,
-        marginTop: 12,
+        width: width * 0.1,
+        height: width * 0.1,
+        marginLeft: width * 0.03,
+        marginTop: height * 0.015,
     },
     cloverProfileImage: {
-        width: 40,
-        height: 40,
-        marginRight: 12,
-        marginTop: 12,
+        width: width * 0.1,
+        height: width * 0.1,
+        marginRight: width * 0.03,
+        marginTop: height * 0.015,
     },
     contentContainer: {
         flexDirection: "column",
         flex: 1,
     },
     itemContainer: {
-        marginBottom: 12,
+        marginBottom: height * 0.015,
     },
     inputContainer: {
         flexDirection: "row",
-        padding: 12,
+        padding: width * 0.03,
         backgroundColor: "#E1EDDA",
-        gap: 10
+        gap: width * 0.025,
     },
     textInput: {
-        height: 45,
-        borderRadius: 40,
-        paddingHorizontal: 12,
+        height: height * 0.06,
+        borderRadius: width * 0.1,
+        paddingHorizontal: width * 0.03,
         backgroundColor: colors.white,
-        flex: 1
-    },    
+        flex: 1,
+    },
     sendButton: {
         backgroundColor: "#258D55",
-        paddingHorizontal: 16,
-        paddingVertical: 10,
-        borderRadius: 40,
+        paddingHorizontal: width * 0.04,
+        paddingVertical: height * 0.015,
+        marginBottom: width * 0.05,
+        borderRadius: width * 0.1,
         alignContent: "center",
         justifyContent: "center",
     },
@@ -64,6 +67,7 @@ const dailyTopicstyles = StyleSheet.create({
         color: "#fff",
         fontWeight: "bold",
         textAlign: "center",
+        fontSize: width * 0.035,
     },
 });
 
