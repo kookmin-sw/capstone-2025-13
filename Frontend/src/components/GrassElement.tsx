9ecddb40e99ac454b4d28cb977ae4e02108dbed6
Kim-JiYun <kimjiyunjane@naver.com>
2025-05-07 09:47:54 +0900

    [Rename] 컴포넌트 이름 변경 #44

--- Frontend/src/components/GrassElement.tsx
+++ Frontend/src/components/GrassElement.tsx
@@ -2,26 +2,23 @@ import React from "react";
 import { Image, View } from "react-native";
 import grassElementStyles, { baseWidth } from "../styles/treeElementStyles";
 // 트리 종류
-type grassType = "one" | "two" | "three";
+type grassType = "one" | "two" | "three"| "four";
 
 interface grassProps {
   type: grassType;
-  title: string;
-  subtitle: string;
 }
 
 export default function Grass({ type }: grassProps) {
 
   const grassImages = {
     one: require("../assets/Images/grass_2.png"),
-    two: require("../assets/Images/grass_3.png"),
-    three: require("../assets/Images/grass_4.png"),
+    two: require("../assets/Images/grass_1.png"),
+    three: require("../assets/Images/grass_3.png"),
+    four: require("../assets/Images/grass_4.png"),
   };
 
   const sizeStyles = {
-    one: { width: 0.3, height: 0.33 },
-    two: { width: 0.37, height: 0.31 },
-    three: { width: 0.38, height: 0.32 },
+    width: 0.5, height: 0.45 
   };
 
 
@@ -32,8 +29,8 @@ export default function Grass({ type }: grassProps) {
           style={[
             grassElementStyles.icon,
             {
-              width: sizeStyles[type].width * baseWidth,
-              height: sizeStyles[type].height * baseWidth,
+              width: sizeStyles.width * baseWidth,
+              height: sizeStyles.height * baseWidth,
             },
           ]}
         />
