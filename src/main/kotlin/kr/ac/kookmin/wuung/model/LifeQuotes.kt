1573661981eda0a4b02cffc30ac1d23e4ea75dfb
toad <toad.mementomori@gmail.com>
2025-05-14 00:41:15 +0900

    [Feat] 명언 API 추가
    데이터베이스에 랜덤한 원소 조회 및 반환

---
+++ src/main/kotlin/kr/ac/kookmin/wuung/model/LifeQuotes.kt
@@ -0,0 +1,32 @@
+package kr.ac.kookmin.wuung.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.PreUpdate
+import jakarta.persistence.Table
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "life_quotes")
+class LifeQuotes (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    val quote: String = "",
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+) {
+    @PreUpdate
+    private fun onUpdate() {
+        updatedAt = LocalDateTime.now()
+    }
+}
