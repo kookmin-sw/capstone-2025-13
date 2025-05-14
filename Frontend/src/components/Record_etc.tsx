f61ee06ef04bf351d2268d1641d4a188ac8c4f04
janeny117 <jsja031017@kookmin.ac.kr>
2025-05-14 23:19:51 +0900

    [Feat] RecordEtc 컴포넌트 리팩토링 및 스타일 수정 #120

--- Frontend/src/components/Record_etc.tsx
+++ Frontend/src/components/Record_etc.tsx
@@ -4,19 +4,17 @@ import styles from "../styles/recordEtcStyles";
 
 export default function RecordEtc() {
     return (
-        <View style={styles.container}>
-                <ImageBackground 
-                    source={require("../assets/Images/record_etc.png")} // 배경 이미지
-                    style={styles.imageBackground}
-                    resizeMode="contain"
-                >
-                    <TextInput 
-                        style={styles.textInput}
-                        placeholder="오늘의 일기를 작성하며 느꼈던 감정이나 더 기록하고 싶은 내용이 있다면 여기에 적어줘! (선택)"
-                        placeholderTextColor="#AFAFAF"
-                        multiline
-                    />
-                </ImageBackground>
+        <View style={styles.shadowWrapper}>  {/* 그림자만 담당 */}
+            <View style={styles.inputBox}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="오늘의 일기를 작성하며 느꼈던 감정이나 더 기록하고 싶은 내용이 있다면 여기에 적어줘! (선택)"
+                    placeholderTextColor="#AFAFAF"
+                    multiline
+                    textAlignVertical="top"
+                />
+            </View>
         </View>
+
     );
 }
