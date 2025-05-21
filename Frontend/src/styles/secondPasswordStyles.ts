1ac4476a3fde104f11ac7cfe976f875677505211
janeny117 <jsja031017@kookmin.ac.kr>
2025-05-21 23:52:36 +0900

    [Feat] 뒤로가기 버튼 추가

--- Frontend/src/styles/secondPasswordStyles.ts
+++ Frontend/src/styles/secondPasswordStyles.ts
@@ -1,5 +1,8 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import fonts from "../constants/fonts";
+
+const { width,  height} = Dimensions.get("window");
+
 
 export const secondPasswordStyles = StyleSheet.create({
     container: {
@@ -7,6 +10,19 @@ export const secondPasswordStyles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
     },
+    header: {
+        marginTop: height * 0.03, 
+        width: "100%",
+    paddingHorizontal: width * 0.05,
+    flexDirection: "row",            // 수평 정렬로 전환
+    alignItems: "center",            // 수직 중앙 정렬
+    justifyContent: "flex-start",  
+    },
+backButtonWrapper: {
+    marginRight: width * 0.02,
+    zIndex: 999,
+    marginLeft: 0,                    // 혹시 불필요한 여백이 있으면 제거
+},
     centerContent: {
         flex: 1,
         justifyContent: 'center',
