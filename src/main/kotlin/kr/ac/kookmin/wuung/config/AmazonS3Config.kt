ad20b9a6b3fdf28263891ff7b1ee09b93f21995d
20203153 <123941587+20203153@users.noreply.github.com>
2025-05-21 20:52:21 +0900

    [debug] debug s3 configs.

--- src/main/kotlin/kr/ac/kookmin/wuung/config/AmazonS3Config.kt
+++ src/main/kotlin/kr/ac/kookmin/wuung/config/AmazonS3Config.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 import java.net.URI
 
@@ -28,6 +29,7 @@ class AmazonS3Config(
 
         return S3Client.builder()
             .credentialsProvider(StaticCredentialsProvider.create(credentials))
+            .region(Region.AP_NORTHEAST_2)
             .endpointOverride(URI.create(s3Url))
             .forcePathStyle(true)
             .build()
