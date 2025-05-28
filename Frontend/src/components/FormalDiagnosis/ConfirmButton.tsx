22327b61dc432319d3703a5f4a1fecdb75dc7881
janeny117 <jsja031017@kookmin.ac.kr>
2025-05-28 22:30:18 +0900

    [Refactor] 모든 질문에 답변하도록 개선 및ConfirmButton 컴포넌트에 비활성화 상태 처리 추가

--- Frontend/src/components/FormalDiagnosis/ConfirmButton.tsx
+++ Frontend/src/components/FormalDiagnosis/ConfirmButton.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
-import styles from "../../styles/FormalDiagnosis/confirmButtonStyles"
+import styles from "../../styles/FormalDiagnosis/confirmButtonStyles";
 
 export default function ConfirmButton({
     label,
     onPress,
+    disabled,
 }: {
     label: string;
     onPress: () => void;
+    disabled?: boolean;
 }) {
     return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.label}>{label}</Text>
+        <TouchableOpacity
+            style={[styles.button, disabled && styles.disabledButton]}
+            onPress={onPress}
+            disabled={disabled}
+        >
+            <Text style={[styles.label, disabled && styles.disabledLabel]}>{label}</Text>
         </TouchableOpacity>
     );
 }
